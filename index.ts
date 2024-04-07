d8a403cd9fd5fd37f5f4975bcd98cd7647095203
Kodzy <75541035+0xKodzy@users.noreply.github.com>
2024-04-07 08:32:06 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import { internal, external, toNano, beginCell, storeMessage, } from '@ton/core'
 import { mnemonicToWalletKey } from '@ton/crypto'
 import { WalletContractV4, TonClient4 } from '@ton/ton';
-import { LiteClient, LiteSingleEngine, LiteRoundRobinEngine } from 'ton-lite-client';
 import { Api, HttpClient } from 'tonapi-sdk-js';
 
 
@@ -15,7 +14,6 @@ function sleep(ms: number) {
 }
 
 type ApiObj = TonClient4
-let liteClient: LiteClient
 
 export function intToIP(int: number) {
     const part1 = int & 255
@@ -26,7 +24,6 @@ export function intToIP(int: number) {
     return `${part4}.${part3}.${part2}.${part1}`
 }
 
-let lc: LiteClient;
 let tonapiClient: Api<unknown>;
 async function getTonapiClient(): Promise<Api<unknown>> {
     if (tonapiClient) {
@@ -50,183 +47,6 @@ async function getTonapiClient(): Promise<Api<unknown>> {
     const client = new Api(httpClient);
     tonapiClient = client
     return client
-}
-
-const createLiteClient = (async () => {
-    // Get JSON config from global.config.json
-    const liteServers = [
-        {
-            "ip": 84478511,
-            "port": 19949,
-            "id": {
-                "@type": "pub.ed25519",
-                "key": "n4VDnSCUuSpjnCyUk9e3QOOd6o0ItSWYbTnW3Wnn8wk="
-            }
-        },
-        {
-            "ip": 84478479,
-            "port": 48014,
-            "id": {
-                "@type": "pub.ed25519",
-                "key": "3XO67K/qi+gu3T9v8G2hx1yNmWZhccL3O7SoosFo8G0="
-            }
-        },
-        {
-            "ip": -2018135749,
-            "port": 53312,
-            "id": {
-                "@type": "pub.ed25519",
-                "key": "aF91CuUHuuOv9rm2W5+O/4h38M3sRm40DtSdRxQhmtQ="
-            }
-        },
-        {
-            "ip": -2018145068,
-            "port": 13206,
-            "id": {
-                "@type": "pub.ed25519",
-                "key": "K0t3+IWLOXHYMvMcrGZDPs+pn58a17LFbnXoQkKc2xw="
-            }
-        },
-        {
-            "ip": -2018145059,
-            "port": 46995,
-            "id": {
-                "@type": "pub.ed25519",
-                "key": "wQE0MVhXNWUXpWiW5Bk8cAirIh5NNG3cZM1/fSVKIts="
-            }
-        },
-        {
-            "ip": 1091931625,
-            "port": 30131,
-            "id": {
-                "@type": "pub.ed25519",
-                "key": "wrQaeIFispPfHndEBc0s0fx7GSp8UFFvebnytQQfc6A="
-            }
-        },
-        {
-            "ip": 1091931590,
-            "port": 47160,
-            "id": {
-                "@type": "pub.ed25519",
-                "key": "vOe1Xqt/1AQ2Z56Pr+1Rnw+f0NmAA7rNCZFIHeChB7o="
-            }
-        },
-        {
-            "ip": 1091931623,
-            "port": 17728,
-            "id": {
-                "@type": "pub.ed25519",
-                "key": "BYSVpL7aPk0kU5CtlsIae/8mf2B/NrBi7DKmepcjX6Q="
-            }
-        },
-        {
-            "ip": 1091931589,
-            "port": 13570,
-            "id": {
-                "@type": "pub.ed25519",
-                "key": "iVQH71cymoNgnrhOT35tl/Y7k86X5iVuu5Vf68KmifQ="
-            }
-        },
-        {
-            "ip": -1539021362,
-            "port": 52995,
-            "id": {
-                "@type": "pub.ed25519",
-                "key": "QnGFe9kihW+TKacEvvxFWqVXeRxCB6ChjjhNTrL7+/k="
-            }
-        },
-        {
-            "ip": -1539021936,
-            "port": 20334,
-            "id": {
-                "@type": "pub.ed25519",
-                "key": "gyLh12v4hBRtyBygvvbbO2HqEtgl+ojpeRJKt4gkMq0="
-            }
-        },
-        {
-            "ip": -1136338705,
-            "port": 19925,
-            "id": {
-                "@type": "pub.ed25519",
-                "key": "ucho5bEkufbKN1JR1BGHpkObq602whJn3Q3UwhtgSo4="
-            }
-        },
-        {
-            "ip": 868465979,
-            "port": 19434,
-            "id": {
-                "@type": "pub.ed25519",
-                "key": "J5CwYXuCZWVPgiFPW+NY2roBwDWpRRtANHSTYTRSVtI="
-            }
-        },
-        {
-            "ip": 868466060,
-            "port": 23067,
-            "id": {
-                "@type": "pub.ed25519",
-                "key": "vX8d0i31zB0prVuZK8fBkt37WnEpuEHrb7PElk4FJ1o="
-            }
-        },
-        {
-            "ip": -2018147130,
-            "port": 53560,
-            "id": {
-                "@type": "pub.ed25519",
-                "key": "NlYhh/xf4uQpE+7EzgorPHqIaqildznrpajJTRRH2HU="
-            }
-        },
-        {
-            "ip": -2018147075,
-            "port": 46529,
-            "id": {
-                "@type": "pub.ed25519",
-                "key": "jLO6yoooqUQqg4/1QXflpv2qGCoXmzZCR+bOsYJ2hxw="
-            }
-        },
-        {
-            "ip": 908566172,
-            "port": 51565,
-            "id": {
-                "@type": "pub.ed25519",
-                "key": "TDg+ILLlRugRB4Kpg3wXjPcoc+d+Eeb7kuVe16CS9z8="
-            }
-        },
-        {
-            "ip": -1185526007,
-            "port": 4701,
-            "id": {
-                "@type": "pub.ed25519",
-                "key": "G6cNAr6wXBBByWDzddEWP5xMFsAcp6y13fXA8Q7EJlM="
-            }
-        }
-    ]
-    const engines: any[] = []
-
-    for (const server of liteServers) {
-        const ls = server
-        engines.push(
-            new LiteSingleEngine({
-                host: `tcp://${intToIP(ls.ip)}:${ls.port}`,
-                publicKey: Buffer.from(ls.id.key, 'base64'),
-            })
-        )
-    }
-
-    const engine = new LiteRoundRobinEngine(engines)
-    const lc2 = new LiteClient({
-        engine,
-        batchSize: 1,
-    })
-    lc = lc2
-})()
-async function getLiteClient(): Promise<LiteClient> {
-    if (lc) {
-        return lc
-    }
-
-    await createLiteClient
-
-    return lc as any
 }
 
 const a = [] as any[]
